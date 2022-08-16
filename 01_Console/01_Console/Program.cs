f6544d229d0d1634fe400c31364c85939a51e497
lse2006 <lse2006@naver.com>
2022-08-16 17:58:23 +0900

    Console_

--- 01_Console/01_Console/Program.cs
+++ 01_Console/01_Console/Program.cs
@@ -10,6 +10,10 @@ namespace _01_Console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");  // "Hello World!"를 출력하는 코드
+            Console.WriteLine("NAME_ Lee Ein Ji");
+            Console.ReadKey();                  // 키 입력 대기하는 코드
+            
         }
     }
 }
